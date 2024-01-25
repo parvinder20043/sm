61d01d46d68d1b8d3a028fc1b4df4379ec654976
Parvinder <22mcc20043@cuchd.in>
2024-01-25 15:33:45 +0530

    Done

---
+++ social_site/static/assets/js/custom.js
@@ -0,0 +1,32 @@
+(function($){
+    "use strict";
+    
+    $(document).ready(function(){
+        
+        $('.sidebar_inner ul li a').on('click', function(e){
+            if($(this).closest("li").children("ul").length) {
+                if ( $(this).closest("li").is(".active-submenu") ) {
+                   $('.sidebar_inner ul li').removeClass('active-submenu');
+                } else {
+                    $('.sidebar_inner ul li').removeClass('active-submenu');
+                    $(this).parent('li').addClass('active-submenu');
+                }
+                e.preventDefault();
+            }
+        });
+        tippy('[data-tippy-placement]', {
+            delay: 100,
+            arrow: true,
+            arrowType: 'sharp',
+            size: 'regular',
+            duration: 200,
+            animation: 'shift-away',
+            animateFill: true,
+            theme: 'dark',
+            distance: 10,   
+        });
+    
+    });
+    
+    })(this.jQuery);
+    
